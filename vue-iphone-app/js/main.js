dca4f7d7fa8cbdccb7c7e99ffa2535e181356ae6
skanderjabbes1 <skanderjabbes2@gmail.com>
2022-02-02 09:55:24 +0100

    new files2

---
+++ vue-iphone-app/js/main.js
@@ -0,0 +1,23 @@
+const myApp = Vue.createApp({
+    data() {
+        return {
+            title: 'iPhone 13',
+            parag: 'Android is better',
+            img: 'assets/images/iphone-13-blue.png',
+            inStock: true,
+            quantity : 6,
+            specs: ['2 Puces', 'Camera', 'HDR'],
+            cart : 0,
+            variants: [
+                { id: 'iphone13-001', color: 'blue', img: './assets/images/iphone-13-blue.png' },
+                { id: 'iphone13-002', color: 'midnight', img: './assets/images/iphone-13-midnight.png' },
+                { id: 'iphone13-003', color: 'red', img: './assets/images/iphone-13-red.png' },
+            ],
+        }
+    },
+    methods: {
+        updateImage(imageUrl) {
+            this.img = imageUrl
+        }
+    }
+}).mount('#app')
